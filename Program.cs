32456447ea9337f59be4226fab4c139cf472b588
Kris Krej <kris@krej.net>
2020-05-22 09:32:50 +0200

    Wczytywanie listy składników

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lab_FoodTracker {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        List<Ingredient> ingredients;
+
+        static void Main() {
+            var thisProgram = new Program();
+            thisProgram.AskPlayerForIngridients();
+            thisProgram.ShowIngredients();
+        }
+
+        void ShowIngredients() {
+            Console.WriteLine("Składniki:");
+            foreach (var ingredient in ingredients)
+                Console.WriteLine(" * " + ingredient.name);
+        }
+
+        void AskPlayerForIngridients() {
+            ingredients = new List<Ingredient>();
+            Console.WriteLine("Wymień po przecinku listę składników");
+            var input = Console.ReadLine();
+            var ingredientNames = input.Split(',');
+            foreach (var name in ingredientNames)
+                ingredients.Add(new Ingredient(name));
+        }
+    }
+
+    public class Ingredient {
+        public string name;
+
+        public Ingredient(string name) {
+            this.name = name.Trim();
         }
     }
 }

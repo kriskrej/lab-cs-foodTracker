b0b515310b9d9175289b2ea459ec205a6c7ea4c6
Kris Krej <kris@krej.net>
2020-05-27 09:10:28 +0200

    finding ingridients by name

--- IngridientDatabase.cs
+++ IngridientDatabase.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace lab_FoodTracker {
     internal class IngridientDatabase {
-        List<Ingredient> ingredients;
+        List<Ingredient> ingredients = new List<Ingredient>() {
+            new Ingredient("kurczak")
+        };
+
         public IngridientDatabase() {
         }
 
-        internal Ingredient Find(string name) {
-            throw new NotImplementedException();
+        public Ingredient Find(string name) {
+            return ingredients.FirstOrDefault(i => i.name == name);
         }
     }
 }

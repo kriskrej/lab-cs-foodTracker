3caa25d9209c9b6f6345f2355514c743fdb27ccd
Kris Krej <kris@krej.net>
2020-05-29 09:55:42 +0200

    cocktails

---
+++ Cocktails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+    static void Main() {
+        var barman = new Barman();
+        barman.HelpUserToComposeCocktail();
+    }
+}
+
+internal class Barman {
+    TheCocktailsDbApi cocktailsDbApi = new TheCocktailsDbApi();
+
+    internal void HelpUserToComposeCocktail() {
+        var ingredients = GetIngredientsFromUser();
+        cocktailsDbApi.FindWithIngredient(ingredients[0]);
+    }
+
+    private List<string> GetIngredientsFromUser() {
+        Console.WriteLine("Podaj listę składników rozdzieloną przecinkami.");
+        var input = Console.ReadLine();
+        var ingredients = input.Split(',');
+        return ingredients.Select(i => i.Trim().ToLowerInvariant()).ToList();
+    }
+}

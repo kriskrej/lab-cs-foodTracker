0ffd6f061f10ec8677a9c041796fbe52b485c622
Kris Krej <kris@krej.net>
2020-06-05 09:45:35 +0200

    percentageDb

---
+++ Cocktails/API/PercentageDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cocktails.API {
+    static class PercentageDB {
+        static int GetAlcoholAmount(string ingredient) {
+            ingredient = ingredient.ToLower();
+
+            if (ingredient == "vodka") return 40;
+            return 0;
+        }
+    }
+}
